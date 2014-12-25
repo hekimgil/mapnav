48bb8becf451e3bebed2855e01911b0995259dba
Hakan Hekimgil <Hakan@Hekimgil.com>
2014-12-26 01:48:45 +0200

    sample user seeding completed...

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(firstName: 'Eden', lastName: 'Hazard', email: 'guest@mapnav.com')
+user1 = User.create(firstName: 'Erman', lastName: 'Yafay', email: 'ermanyafay@mapnav.com')
+event11 = Event.create(ownerUser: user1[:id], createdDate: DateTime.now, title: "Erman'ın fotoları", description: "İYTE'den muhtelif fotoğraf manzaraları...", thumbnail: "http://assets-s3.usmagazine.com/uploads/assets/celebrities/238-brad-pitt/1250530894_brad_pitt_290x402.jpg", latitude: 38.3947403, longitude: 26.9997663)
+photo11 = Photo.create(ownerEvent: event11[:id], url: "http://i.imgur.com/XLLGCqW.jpg", thumbnail: "https://drive.google.com/file/d/0BwvugTdznRN8RHdYMExRRXRPSGc/view?usp=sharing", title: 'In the lab', description: 'Working on CENG316 project...', latitude: 38.3947403, longitude: 26.9997663, createdDate: DateTime.now)
+
+user2 = User.create(firstName: 'Eden', lastName: 'Hazard', email: 'edenhazard@mapnav.com')
+event21 = Event.create(ownerUser: user2[:id], createdDate: DateTime.now, title: "Koning Boudewijnstadion", description: "Nationale team spelen bij Koning Boudewijnstadion", thumbnail: "http://photos.wikimapia.org/p/00/00/40/77/08_75.jpg", latitude: 50.895956, longitude: 4.334102)
+event22 = Event.create(ownerUser: user2[:id], createdDate: DateTime.now, title: "Stamford Bridge", description: "This is where I play for Chelsea FC", thumbnail: "http://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Stamford_Bridge_-_West_Stand.jpg/120px-Stamford_Bridge_-_West_Stand.jpg", latitude: 51.48167, longitude: -0.19111)
+photo21 = Photo.create(ownerEvent: event21[:id], url: "http://www.belgianfootball.be/nl/sites/default/files/imagecache/carrousel_content/beliceitp.jpg", thumbnail: "http://www.belgianfootball.be/nl/sites/default/files/imagecache/carrousel_content/beliceitp.jpg", title: 'België wint!!!', description: 'België wint met 3-1 van IJsland', latitude: 50.895956, longitude: 4.334102, createdDate: DateTime.now)
+photo22 = Photo.create(ownerEvent: event22[:id], url: "http://i.telegraph.co.uk/multimedia/archive/02788/eden-hazard_2788463b.jpg", thumbnail: "http://i.telegraph.co.uk/multimedia/archive/02788/eden-hazard_2788463b.jpg", title: 'And he scores!!!', description: 'I like scoring...', latitude: 51.48167, longitude: -0.19111, createdDate: DateTime.now)
+photo23 = Photo.create(ownerEvent: event21[:id], url: "http://r.ldh.be/image/ca/51b73737e4b0de6db975d8ca.jpg", thumbnail: "http://r.ldh.be/image/ca/51b73737e4b0de6db975d8ca.jpg", title: 'Eden', description: 'Nationale team', latitude: 50.895956, longitude: 4.334102, createdDate: DateTime.now)
+photo24 = Photo.create(ownerEvent: event22[:id], url: "http://i.telegraph.co.uk/multimedia/archive/02775/eden-hazard-epa_2775295b.jpg", thumbnail: "http://i.telegraph.co.uk/multimedia/archive/02775/eden-hazard-epa_2775295b.jpg", title: 'Good shot!', description: 'Nice goal against Swansea...', latitude: 51.48167, longitude: -0.19111, createdDate: DateTime.now)
+
+
+
+user3 = User.create(firstName: 'Acun', lastName: 'Ilıcalı', email: 'acunilicali@mapnav.com')
+
+
+
+
+
